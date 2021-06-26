a18dda081ea0a1e1a88ad7133bbbb625f1343cb1
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-06-26 08:58:12 +0530

    Renamed the app

--- android/app/src/main/java/com/edumobileapp/MainActivity.java
+++ android/app/src/main/java/com/edumobileapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.eduapp;
+package com.edumobileapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "eduApp";
+    return "eduMobileApp";
   }
 }
--- ios/eduMobileAppTests/eduMobileAppTests.m
+++ ios/eduMobileAppTests/eduMobileAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface eduAppTests : XCTestCase
+@interface eduMobileAppTests : XCTestCase
 
 @end
 
-@implementation eduAppTests
+@implementation eduMobileAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
